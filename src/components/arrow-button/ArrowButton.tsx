744adf179a9387436155dcaad2aabdd2ffe0f838
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-06-08 21:17:42 +0300

    Описаны пропсы, добавлена обработка классов. Изменение состояния удалено (перемещено в родительский компонент

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,28 +1,39 @@
 import { useState } from 'react';
+import clsx from 'clsx';
 
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
 
+
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
-	const [isOpen, toggleState] = useState(false);
+type ArrowButtonProps = {
+	handleClick: OnClick;
+	isOpen: boolean;
+}
 
-	const hanldeClick: OnClick = () => {
-		toggleState(!isOpen);
-	}
+export const ArrowButton = (props: ArrowButtonProps) => {
+	const containerCls = clsx({
+		[styles.container]: true,
+		[styles.container_open]: props.isOpen
+	})
+
+	const imgCls = clsx({
+		[styles.arrow]: !props.isOpen,
+		[styles.arrow_open]: props.isOpen
+	});
 
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
-			onClick={hanldeClick}
+			onClick={props.handleClick}
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={isOpen ? styles.arrow_open : styles.arrow} />
+			className={containerCls}>
+			<img src={arrow} alt='иконка стрелочки' className={imgCls} />
 		</div>
 	);
 };
