bb74d33d61330de10c671af92df71712f38fc250
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-06-08 21:19:58 +0300

    Добавлено изменение состояния и обработка имен классов

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,30 @@
+import { useState } from 'react';
+import clsx from 'clsx';
+
 import { ArrowButton } from 'components/arrow-button';
+import { OnClick } from '../arrow-button/ArrowButton';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
 
+
+
 export const ArticleParamsForm = () => {
+	const [isOpen, toggleState] = useState(false);
+
+	const handleToggleState: OnClick = () => {
+		toggleState(!isOpen);
+	}
+
+	const asideCls = clsx({
+		[styles.container]: true,
+		[styles.container_open]: isOpen
+	});
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton handleClick={handleToggleState} isOpen={isOpen} />
+			<aside className={asideCls}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
