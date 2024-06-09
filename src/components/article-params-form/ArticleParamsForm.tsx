10d420372782928ec285c0ae87526ab776f0a59e
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-06-09 12:14:04 +0300

    Добавлена разметка формы и обработка состояний элементов формы

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -4,6 +4,11 @@ import clsx from 'clsx';
 import { ArrowButton } from 'components/arrow-button';
 import { OnClick } from '../arrow-button/ArrowButton';
 import { Button } from 'components/button';
+import { Select } from '../select';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
+
+import { fontFamilyOptions, fontSizeOptions, fontColors, backgroundColors, contentWidthArr } from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
 
@@ -11,6 +16,11 @@ import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
 	const [isOpen, toggleState] = useState(false);
+	const [selectedFontFamily, setFontFamily] = useState(fontFamilyOptions[0]);
+	const [selectedFontSize, setFontSize] = useState(fontSizeOptions[0]);
+	const [selectedFontColor, setFontColor] = useState(fontColors[0]);
+	const [selectedBgColor, setBgColor] = useState(backgroundColors[0]);
+	const [selectedContentWidth, setContentWidth] = useState(contentWidthArr[0]);
 
 	const handleToggleState: OnClick = () => {
 		toggleState(!isOpen);
@@ -26,6 +36,18 @@ export const ArticleParamsForm = () => {
 			<ArrowButton handleClick={handleToggleState} isOpen={isOpen} />
 			<aside className={asideCls}>
 				<form className={styles.form}>
+					<Select title='Шрифт' selected={selectedFontFamily} options={fontFamilyOptions} onChange={setFontFamily} ></Select>
+
+					<RadioGroup title='Размер шрифта' name='' selected={selectedFontSize} options={fontSizeOptions} onChange={setFontSize}></RadioGroup>
+
+					<Select title='Цвет шрифта' selected={selectedFontColor} options={fontColors} onChange={setFontColor} ></Select>
+
+					<Separator></Separator>
+
+					<Select title='Цвет фона' selected={selectedBgColor} options={backgroundColors} onChange={setBgColor} ></Select>
+
+					<Select title='Ширина контента' selected={selectedContentWidth} options={contentWidthArr} onChange={setContentWidth}></Select>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
