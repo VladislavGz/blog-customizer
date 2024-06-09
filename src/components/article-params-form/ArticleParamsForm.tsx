f012e064762f9989c4ec5535c59f66fc9b7f1320
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-06-09 23:48:24 +0300

    Исправлены замечания ревью

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -9,33 +9,32 @@ import { Select } from '../select';
 import { RadioGroup } from '../radio-group';
 import { Separator } from '../separator';
 
-import { fontFamilyOptions, fontSizeOptions, fontColors, backgroundColors, contentWidthArr, OptionType } from 'src/constants/articleProps';
+import { fontFamilyOptions, fontSizeOptions, fontColors, backgroundColors, contentWidthArr, OptionType, ArticleStateType, defaultArticleState } from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
-import { TSettings } from 'src/index';
 import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
 
 export type TFormProps = {
-	currentSettings: TSettings;
-	applySettingsFunc: (settings: TSettings) => void;
+	currentSettings: ArticleStateType;
+	applySettingsFunc: (settings: ArticleStateType) => void;
 }
 
 export const ArticleParamsForm = ({ currentSettings, applySettingsFunc }: TFormProps) => {
 	const rootRef = useRef<HTMLDivElement>(null);
-	const [isOpen, toggleState] = useState(false);
-	const [formState, setFormState] = useState<TSettings>(currentSettings)
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [formState, setFormState] = useState<ArticleStateType>(currentSettings)
 
 	useOutsideClickClose({
-		isOpen: isOpen,
+		isOpen: isMenuOpen,
 		rootRef,
-		onChange: toggleState
+		onChange: setIsMenuOpen
 	})
 
 	const handleToggleState: OnClick = () => {
-		toggleState(!isOpen);
+		setIsMenuOpen(!isMenuOpen);
 	}
 
-	const handleChange = (type: keyof TSettings) => (selected: OptionType) => {
+	const handleChange = (type: keyof ArticleStateType) => (selected: OptionType) => {
 		setFormState(prevState => ({
 			...prevState,
 			[type]: selected
@@ -43,35 +42,36 @@ export const ArticleParamsForm = ({ currentSettings, applySettingsFunc }: TFormP
 	}
 
 	const handleResetEvent = () => {
-		setFormState(currentSettings);
+		setFormState(defaultArticleState);
+		applySettingsFunc(defaultArticleState);
 	}
 
 	const handleSubmitEvent = (evt: React.FormEvent) => {
 		evt.preventDefault()
-		applySettingsFunc(formState)
+		applySettingsFunc(formState);
 	}
 
 	const asideCls = clsx({
 		[styles.container]: true,
-		[styles.container_open]: isOpen
+		[styles.container_open]: isMenuOpen
 	});
 
 	return (
 		<div ref={rootRef}>
-			<ArrowButton handleClick={handleToggleState} isOpen={isOpen} />
+			<ArrowButton handleClick={handleToggleState} isOpen={isMenuOpen} />
 			<aside className={asideCls}>
 				<form className={styles.form} onSubmit={handleSubmitEvent}>
 					<Text as={'h2'} weight={800} size={31} uppercase>Задайте параметры</Text>
 
-					<Select title='Шрифт' selected={formState.fontFamily} options={fontFamilyOptions} onChange={handleChange('fontFamily')}></Select>
+					<Select title='Шрифт' selected={formState.fontFamilyOption} options={fontFamilyOptions} onChange={handleChange('fontFamilyOption')}></Select>
 
-					<RadioGroup title='Размер шрифта' name='FontSize' selected={formState.fontSize} options={fontSizeOptions} onChange={handleChange('fontSize')} ></RadioGroup>
+					<RadioGroup title='Размер шрифта' name='FontSize' selected={formState.fontSizeOption} options={fontSizeOptions} onChange={handleChange('fontSizeOption')} ></RadioGroup>
 
 					<Select title='Цвет шрифта' selected={formState.fontColor} options={fontColors} onChange={handleChange('fontColor')}></Select>
 
 					<Separator></Separator>
 
-					<Select title='Цвет фона' selected={formState.bgColor} options={backgroundColors} onChange={handleChange('bgColor')}></Select>
+					<Select title='Цвет фона' selected={formState.backgroundColor} options={backgroundColors} onChange={handleChange('backgroundColor')}></Select>
 
 					<Select title='Ширина контента' selected={formState.contentWidth} options={contentWidthArr} onChange={handleChange('contentWidth')}></Select>
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,49 +4,30 @@ import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { OptionType, defaultArticleState } from './constants/articleProps';
+import { ArticleStateType, OptionType, defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
-
-export type TSettings = {
-	fontFamily: OptionType;
-	fontSize: OptionType;
-	fontColor: OptionType;
-	bgColor: OptionType;
-	contentWidth: OptionType;
-}
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
-	const [settings, setSettings] = useState<TSettings>({
-		fontFamily: defaultArticleState.fontFamilyOption,
-		fontSize: defaultArticleState.fontSizeOption,
-		fontColor: defaultArticleState.fontColor,
-		bgColor: defaultArticleState.backgroundColor,
-		contentWidth: defaultArticleState.contentWidth
-	});
-
-	const applySettings = (settings: TSettings) => {
-		setSettings(settings);
-	}
-
+	const [settings, setSettings] = useState<ArticleStateType>(defaultArticleState);
 
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{ 
-					'--font-family': settings.fontFamily.value,
-					'--font-size': settings.fontSize.value,
+					'--font-family': settings.fontFamilyOption.value,
+					'--font-size': settings.fontSizeOption.value,
 					'--font-color': settings.fontColor.value,
 					'--container-width': settings.contentWidth.value,
-					'--bg-color': settings.bgColor.value,
+					'--bg-color': settings.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm currentSettings={settings} applySettingsFunc={applySettings}/>
+			<ArticleParamsForm currentSettings={settings} applySettingsFunc={setSettings}/>
 			<Article />
 		</div>
 	);
